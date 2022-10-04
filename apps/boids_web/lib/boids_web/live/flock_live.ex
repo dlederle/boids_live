c999e292249a8c0efe430009df796eb2b310ed60
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2022-10-04 13:50:59 -0400

    Add tick rate knob

--- apps/boids_web/lib/boids_web/live/flock_live.ex
+++ apps/boids_web/lib/boids_web/live/flock_live.ex
@@ -17,11 +17,15 @@ defmodule BoidsWeb.FlockLive do
     <canvas width={width} height={height} phx-hook="canvas" id="flock" phx-update="ignore" style="border: 1px solid black;">
       Canvas not supported!
     </canvas>
+    <form phx-change="set_tick_rate">
+      <label for="tick_rate">Tick Rate</label>
+      <input type="range" name="tick_rate" id="tick_rate" min="0" max="100" value={@tick_rate} phx-debounce="1000">
+    </form>
     """
   end
 
   def mount(_params, _session, socket) do
-    tick()
+    tick(@tick_rate_ms)
 
     flock =
       [
@@ -30,32 +34,21 @@ defmodule BoidsWeb.FlockLive do
         Boid.new(20, 10),
         Boid.new(10, 10),
         Boid.new(250, 250)
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250),
-        # Boid.new(250, 250)
       ]
       |> Flock.new()
 
     {:ok,
      socket
+     |> assign(:tick_rate, @tick_rate_ms)
      |> assign(:flock, flock)}
   end
 
-  def handle_info(:tick, %{assigns: %{flock: flock}} = socket) do
-    tick()
+  def handle_event("set_tick_rate", %{"_target" => ["tick_rate"], "tick_rate" => rate}, socket) do
+    {:noreply, assign(socket, :tick_rate, String.to_integer(rate))}
+  end
+
+  def handle_info(:tick, %{assigns: %{flock: flock, tick_rate: tick_rate}} = socket) do
+    tick(tick_rate)
 
     moved_flock =
       flock
@@ -68,7 +61,7 @@ defmodule BoidsWeb.FlockLive do
     {:noreply, push_event(updated_socket, "render_boids", %{boids: boids})}
   end
 
-  defp tick(), do: Process.send_after(self(), :tick, @tick_rate_ms)
+  defp tick(tick_rate), do: Process.send_after(self(), :tick, tick_rate)
 
   # Enforce canvas borders, wrap boid around to the other side
   defp wrap(boid) do
